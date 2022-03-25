af5fa751e322b096dd4aef37f82d7ad7b6664275
Tongrakroj <krit.tongrakroj@accenture.com>
2022-03-25 11:15:40 +0700

    .

--- Deployment-Scripts/rollback/deploy-rollback.py
+++ Deployment-Scripts/rollback/deploy-rollback.py
@@ -25,9 +25,10 @@ print ("selected commit: ", commitid)
 
 changed_files_firstcommit = []
 changed_files_lastcommit = []
+
 changed_files = []
 
-for x in commits_list[0].diff(commitid):
+for x in commits_list[0].diff(commits_list[-1]):
     if x.a_blob.path not in changed_files:
         changed_files.append(x.a_blob.path)
         
@@ -36,8 +37,9 @@ for x in commits_list[0].diff(commitid):
         
 print (changed_files_firstcommit = [])
 print (changed_files_lastcommit = [])
+print(changed_files)
 
-for info in changed_files_firstcommit:
+for info in changed_files:
         #if file_path.startswith(('dp-bt', 'dp-rlt')):
         #env_config_path = f'{config.REPO_NOTEBOOKS_DIRECTORY}/fw/cmmn/config/environment_config' # Deploy every time
         if info.startswith(repo_path):
@@ -47,12 +49,3 @@ for info in changed_files_firstcommit:
             print("not interest")
             print(info)
 
-for info in changed_files_lastcommit:
-        #if file_path.startswith(('dp-bt', 'dp-rlt')):
-        #env_config_path = f'{config.REPO_NOTEBOOKS_DIRECTORY}/fw/cmmn/config/environment_config' # Deploy every time
-        if info.startswith(repo_path):
-            print("in specified folder")
-            print(info)
-        else:
-            print("not interest")
-            print(info)
