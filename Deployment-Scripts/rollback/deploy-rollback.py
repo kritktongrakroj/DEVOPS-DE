24e1cf355300b8211ffb7de628b258a8751c92bc
Tongrakroj <krit.tongrakroj@accenture.com>
2022-03-28 20:13:24 +0700

    .

--- Deployment-Scripts/rollback/deploy-rollback.py
+++ Deployment-Scripts/rollback/deploy-rollback.py
@@ -23,36 +23,36 @@ print(repo)
 
 
 
-commits_list = list(repo.iter_commits(branchname,max_count=20))
+#commits_list = list(repo.iter_commits(branchname,max_count=20))
 commitidfromtag = repo.commit(inputtag)
 repodif = repo.git.diff(commitidfromtag)
 print(repodif)
 
 
 #print(commits_list)
-print ("last commit: ", commits_list[-1])
+#print ("last commit: ", commits_list[-1])
 print ("rollback selected commit: ", commitidfromtag)
 
 
 changed_files = set()
 notebook_change_file = []
 
-print(commits_list[0].diff(commits_list[-1]))
+#print(commits_list[0].diff(commits_list[-1]))
 
-for x in commits_list[0].diff(commits_list[-1]):
-    if x.a_blob.path:
-        changed_files.add(x.a_blob.path)
+#for x in commits_list[0].diff(commits_list[-1]):
+    #if x.a_blob.path:
+        #changed_files.add(x.a_blob.path)
         
-    if x.b_blob.path:
-        changed_files.add(x.b_blob.path)
+    #if x.b_blob.path:
+        #changed_files.add(x.b_blob.path)
         
-print(changed_files)
+#print(changed_files)
 
-for info in changed_files:
-        if info.startswith(notebook_path):
-            print("in specified folder")
-            print(info)
-            notebook_change_file.append(info)
+#for info in changed_files:
+#        if info.startswith(notebook_path):
+#            print("in specified folder")
+#            print(info)
+#            notebook_change_file.append(info)
 
 
-print(notebook_change_file)
+#print(notebook_change_file)
