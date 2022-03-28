1b14be934440e500ffd7851c22c6d983476af238
Tongrakroj <krit.tongrakroj@accenture.com>
2022-03-28 18:06:53 +0700

    .

--- Deployment-Scripts/rollback/deploy-rollback.py
+++ Deployment-Scripts/rollback/deploy-rollback.py
@@ -11,6 +11,7 @@ import base64
 inputtag = str(sys.argv[1])
 repo_url = str(sys.argv[2])
 repo_path = str(sys.argv[3])
+notebook_path = str(sys.argv[4])
 
 
 print(repo_path)
@@ -43,12 +44,9 @@ print(commits_list)
 print ("last commit: ", commits_list[-1])
 print ("rollback selected commit: ", commitidfromtag)
 
-changed_files_firstcommit = []
-changed_files_lastcommit = []
 
 changed_files = []
-
-print(commits_list[0].diff(commitidfromtag))
+notebook_change_file = []
 
 for x in commits_list[0].diff(commitidfromtag):
     if x.a_blob.path not in changed_files:
@@ -57,15 +55,13 @@ for x in commits_list[0].diff(commitidfromtag):
     if x.b_blob is not None and x.b_blob.path not in changed_files:
         changed_files.append(x.b_blob.path)
         
-print (changed_files_firstcommit)
-print (changed_files_lastcommit)
 print(changed_files)
 
 for info in changed_files:
-        #if file_path.startswith(('dp-bt', 'dp-rlt')):
-        if info.startswith(repo_path):
+        if info.startswith(notebook_path):
             print("in specified folder")
             print(info)
-        else:
-            print("not interest")
-            print(info)
+            notebook_change_file.append(info)
+
+
+print(notebook_change_file)
