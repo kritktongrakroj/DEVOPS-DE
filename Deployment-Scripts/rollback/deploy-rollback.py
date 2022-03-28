d3e118ea218d970b51b0c6aaea52651414366644
Tongrakroj <krit.tongrakroj@accenture.com>
2022-03-28 20:53:47 +0700

    .

--- Deployment-Scripts/rollback/deploy-rollback.py
+++ Deployment-Scripts/rollback/deploy-rollback.py
@@ -51,34 +51,23 @@ def find_seq():
 
 seq = find_seq()
 
-
+# show all commit list in branch
 print(commits_list)
 
 #print ("last commit: ", commits_list[-1])
 #print ("rollback selected commit: ", commitidfromtag)
 
 
-changed_files = []
-notebook_change_file = []
-
-for x in commits_list[0].diff(commitidfromtag):
-    if x.a_blob.path not in changed_files:
-        changed_files.append(x.a_blob.path)
-        
-    if x.b_blob is not None and x.b_blob.path not in changed_files:
-        changed_files.append(x.b_blob.path)
-
-print (changed_files)
 
 
-for i in range(len(changed_files)):
+#for i in range(len(changed_files)):
         #matching tag
         #print(taglist[i])
-        path = str(changed_files[i])
-        if path.startswith(notebook_path):
-            print("Hello this is path match")
-            print (path)
-            break
+        #path = str(changed_files[i])
+        #if path.startswith(notebook_path):
+            #print("Hello this is path match")
+            #print (path)
+            #break
 
 #for info in changed_files:
 #        if info.startswith(notebook_path):
