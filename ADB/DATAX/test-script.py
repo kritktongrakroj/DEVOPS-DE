4b7fd268d25a89b41533d001543d29c3ff9452be
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-07 17:56:10 +0700

    Update test-script.py

--- ADB/DATAX/test-script.py
+++ ADB/DATAX/test-script.py
@@ -1,5 +1,10 @@
-print("Hello, World!")
+# Encryption functions 
+spark.sql(f""" CREATE FUNCTION IF NOT EXISTS encrypt AS 'AESEncryptedUDF'
+                USING JAR '/Jars/jars/{require_jar_dict["AESEncryption_UDF_V3_3_0"]}' """) 
+
+spark.sql(f""" CREATE FUNCTION IF NOT EXISTS decrypt AS 'AESDecryptedUDF'
+                USING JAR '/Jars/jars/{require_jar_dict["AESEncryption_UDF_V3_3_0"]}' """)
+
 
 # COMMAND ----------
-
 dbutils.notebook.exit("SUCCESS")
