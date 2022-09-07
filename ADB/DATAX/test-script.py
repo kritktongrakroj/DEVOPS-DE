c2ab3dedc55fd54b6f6f2264cac3e75686cd5628
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-07 18:16:52 +0700

    Update test-script.py

--- ADB/DATAX/test-script.py
+++ ADB/DATAX/test-script.py
@@ -1,3 +1,19 @@
+# Databricks notebook source
+jar_file_list = dbutils.fs.ls("FileStore/jars/")
+require_jar_dict = {}
+
+for jar_file in jar_file_list:
+    if "decodeTiHex2Str_UDF_V1_1_1" in jar_file[1]:
+        require_jar_dict['decodeTiHex2Str_UDF_V1_1_1'] = jar_file[1]
+    elif "AESEncryptionRotator_UDF_V1_0_1" in jar_file[1]:
+        require_jar_dict['AESEncryptionRotator_UDF_V1_0_1'] = jar_file[1]
+    elif "AESEncryption_UDF_V3_3_0" in jar_file[1]:
+        require_jar_dict['AESEncryption_UDF_V3_3_0'] = jar_file[1]
+    elif "CustomDateTime_Convertor_UDF_V2_1_0" in jar_file[1]:
+        require_jar_dict['CustomDateTime_Convertor_UDF_V2_1_0'] = jar_file[1]
+
+# COMMAND ----------
+
 # Encryption functions 
 spark.sql(f""" CREATE FUNCTION IF NOT EXISTS encrypt AS 'AESEncryptedUDF'
                 USING JAR '/Jars/jars/{require_jar_dict["AESEncryption_UDF_V3_3_0"]}' """) 
