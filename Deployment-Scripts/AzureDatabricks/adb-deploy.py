a9182cfebbb50a1a5799ed4a42a711e2d2a71c07
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-07 16:49:43 +0700

    Update adb-deploy.py

--- Deployment-Scripts/AzureDatabricks/adb-deploy.py
+++ Deployment-Scripts/AzureDatabricks/adb-deploy.py
@@ -1,3 +1,4 @@
+from asyncore import file_dispatcher
 import sys
 import os
 from os.path import isfile, join
@@ -5,7 +6,9 @@ from os import listdir
 import base64
 import glob
 import requests
-#########################
+import pathlib
+
+print(sys.argv)
 
 tenant_id= str(sys.argv[1])
 client_id=str(sys.argv[2])
@@ -13,14 +16,17 @@ client_secret=str(sys.argv[3])
 subscription_id=str(sys.argv[4])
 resourceGroup=str(sys.argv[5])
 workspaceName=str(sys.argv[6])
+
+
 NOTEBOOK_DIRECTORY=str(sys.argv[7])
 DATABRICKS_NOTEBOOKS_DIRECTORY = str(sys.argv[8])
 
-azure_databricks_resource_id="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
-resourceId= '/subscriptions/'+subscription_id+'/resourceGroups/'+resourceGroup+'/providers/Microsoft.Databricks/workspaces/'+workspaceName
-#print("the valie is",resourceId,azure_databricks_resource_id)
+
+to_add = str(sys.argv[9])
+print(to_add)
 
 
+#Declare REQ BODY for dbrks_bearer_token() and dbrks_management_token()
 TOKEN_REQ_BODY = {
     'grant_type': 'client_credentials',
     'client_id':  client_id,
@@ -28,17 +34,8 @@ TOKEN_REQ_BODY = {
 TOKEN_BASE_URL = 'https://login.microsoftonline.com/' +  tenant_id + '/oauth2/token'
 TOKEN_REQ_HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-
-def dbrks_management_token():
-        TOKEN_REQ_BODY['resource'] = 'https://management.core.windows.net/'
-        response = requests.get(TOKEN_BASE_URL, headers=TOKEN_REQ_HEADERS, data=TOKEN_REQ_BODY)
-        if response.status_code == 200:
-            print(response.status_code)
-        else:
-            raise Exception(response.text)
-        return response.json()['access_token']
-
-
+# Function for getting for the global Databricks application.
+# The resource name is fixed and never changes.
 def dbrks_bearer_token():
         TOKEN_REQ_BODY['resource'] = '2ff814a6-3304-4ab8-85cb-cd0e6f879c1d'
         response = requests.get(TOKEN_BASE_URL, headers=TOKEN_REQ_HEADERS, data=TOKEN_REQ_BODY)
@@ -48,39 +45,48 @@ def dbrks_bearer_token():
             raise Exception(response.text)
         return response.json()['access_token']
 
+# Function for getting a token for the Azure management API
+def dbrks_management_token():
+        TOKEN_REQ_BODY['resource'] = 'https://management.core.windows.net/'
+        response = requests.get(TOKEN_BASE_URL, headers=TOKEN_REQ_HEADERS, data=TOKEN_REQ_BODY)
+        if response.status_code == 200:
+            print(response.status_code)
+        else:
+            raise Exception(response.text)
+        return response.json()['access_token']
+
+# Get a token for the global Databricks application.
+# Shell script -> token=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d | jq -r .accessToken)
 DBRKS_BEARER_TOKEN = dbrks_bearer_token()
+
+# Get a token for the Azure management API
 DBRKS_MANAGEMENT_TOKEN = dbrks_management_token()
 
+###########################################################################################################
 
+#Declare REQ BODY for getting ADB workspace URL 
 DBRKS_WorksID_REQ_HEADERS = {
     'Authorization': 'Bearer ' + DBRKS_MANAGEMENT_TOKEN,
     'Content-Type': 'application/json'}
 
-#Management URL
+#Get ADB workspace URL 
 mgmt_url="https://management.azure.com/subscriptions/"+subscription_id+"/resourcegroups/"+resourceGroup+"/providers/Microsoft.Databricks/workspaces/"+workspaceName+"?api-version=2018-04-01"
 response = requests.get(mgmt_url,headers=DBRKS_WorksID_REQ_HEADERS)
-Workspcae_URL=response.json()['properties']['workspaceUrl']
+Workspcae_URL = response.json()['properties']['workspaceUrl']
+print("ADB Workspace URL: "+Workspcae_URL)
 
-# Create Folder is does not exist
+###########################################################################################################
+# Get Databrick workspace token.
+ADB_TOKEN_REQ_BODY ={
+    'lifetime_seconds': 3600, 
+    'comment': 'this is an example token'
+}
 
-
-dbrks_create_dir_url =  "https://"+Workspcae_URL+".azuredatabricks.net/api/2.0/workspace/mkdirs"
-dbrks_import_rest_url = "https://"+Workspcae_URL+".azuredatabricks.net/api/2.0/workspace/import"
-api_url="https://"+Workspcae_URL+".azuredatabricks.net/api/"
-
-#print(dbrks_import_rest_url)
-#print(dbrks_create_dir_url)
-
-DBRKS_REQ_HEADERS = {
-    'Authorization': 'Bearer ' +DBRKS_BEARER_TOKEN,
-    'X-Databricks-Azure-Workspace-Resource-Id': '/subscriptions/'+ subscription_id +'/resourceGroups/'+resourceGroup+'/providers/Microsoft.Databricks/workspaces/' + workspaceName,
-    'X-Databricks-Azure-SP-Management-Token': DBRKS_MANAGEMENT_TOKEN}
-
-
-def adb_notebook_api(endpoint,json_content):
+def create_adb_token_api(endpoint,json_content):
     api_url = "https://{}/api/{}".format(Workspcae_URL,endpoint)
     headers = {'Authorization': 'Bearer {}'.format(DBRKS_BEARER_TOKEN)}
-    
+    print(api_url)
+
     response = requests.post(
         api_url,
         headers=headers,
@@ -91,18 +97,48 @@ def adb_notebook_api(endpoint,json_content):
         raise Exception("API Failed, Result: {}".format(response.json()))    
         response.raise_for_status()
     return True
-    
+
+
+Workspcae_Token = create_adb_token_api("2.0/token/create",ADB_TOKEN_REQ_BODY)
+print(Workspcae_Token)
+if not Workspcae_Token:
+    raise Exception("Create adb token failed")
+
+##################################################
+# Begin create folder that relate to changed
+
+
 def createfolder_to_databricks(notebook_path):
+
     # Create Directory in Databricks Workspace if has directory
     if os.path.dirname(notebook_path) != "/" and os.path.dirname(notebook_path):
         dir_name = os.path.split(notebook_path)[0]
-        json_notebook_path = {
-            "path" : dir_name
-        }
-        status = adb_notebook_api("2.0/workspace/mkdirs",json_notebook_path)
-        if not status:
-            raise Exception("Create directory {} failed".format(dir_name))
+        print(dir_name)
+        if dir_name == DATABRICKS_NOTEBOOKS_DIRECTORY:
+            print("Cannot create root directory, will duplicate it")
+            
+        else:
+            json_notebook_path = {
+                "path" : dir_name
+            }
+            status = create_adb_token_api("2.0/workspace/mkdirs",json_notebook_path)
+            if not status:
+                raise Exception("Create directory {} failed".format(dir_name))
+    
 
+# Function to remove file
+def delete_existing_notebook_directory(notebook_path):
+    json_content = {
+        "path": notebook_path,
+        "recursive": True
+    }
+    status = create_adb_token_api("2.0/workspace/delete",json_content)
+    print(status)
+    if not status:
+        raise Exception("Cannot delete {} .".format(notebook_path))
+
+
+#Function import notebook
 def import_to_databricks(notebook_path,full_path_file):
     file_content = open(full_path_file, "r").read()
     content_bytes = file_content.encode('utf-8')
@@ -128,54 +164,44 @@ def import_to_databricks(notebook_path,full_path_file):
     } 
     
     print("Start import {}.".format(filename))
-    status = adb_notebook_api("2.0/workspace/import",json_content)
+    status = create_adb_token_api("2.0/workspace/import",json_content)
     if not status:
         raise "Import {} failed".format(filename)
     return "Import {} success".format(filename)
 
 
-
 DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), DATABRICKS_NOTEBOOKS_DIRECTORY)
 TYPE_OF_FILE = "."
+print(DIRECTORY)
+print(DATABRICKS_NOTEBOOKS_DIRECTORY)              
 
-            
-#Create Folder again
-if os.path.exists(DIRECTORY):
-        for path, subdirs, files in os.walk(DIRECTORY):
-                notebook_full_path_dir = path.split(DATABRICKS_NOTEBOOKS_DIRECTORY)[-1]
-                notebook_name_dir = notebook_full_path_dir[1:]
-                notebook_abs_path_dir = os.path.join(NOTEBOOK_DIRECTORY,notebook_name_dir)
-                dir_name = notebook_abs_path_dir.replace('\\','/')
-                print("Dir Name is",dir_name)
-                json_notebook_path = {
-                "path" : dir_name
-                }
-                print(json_notebook_path)
-                status = adb_notebook_api("2.0/workspace/mkdirs",json_notebook_path)
-                if not status:
-                   raise Exception("Create directory {} failed".format(dir_name))
-
-                
+to_add = to_add.split(',')                
+print(f'splited to_add: {to_add}')
+#create new folder for change file if required
+if os.listdir(DATABRICKS_NOTEBOOKS_DIRECTORY):
+    print("Begin create folder on the changed file")
+    for folder_to_create in to_add:
+        split_remove_path_folder_add = pathlib.Path(folder_to_create)
+        raw_path_folder_add = pathlib.Path(*split_remove_path_folder_add.parts[3:])
+        join_raw_path_add = os.path.join(NOTEBOOK_DIRECTORY, raw_path_folder_add)
+        print(join_raw_path_add)
+        response = createfolder_to_databricks(join_raw_path_add)
 
 
-#create Files
-if os.path.exists(DIRECTORY):
-        for path, subdirs, files in os.walk(DIRECTORY):
-            for file in files:
-                notebook_full_path = os.path.join(path.split(DATABRICKS_NOTEBOOKS_DIRECTORY)[-1], file)
-                notebook_name = notebook_full_path.split(TYPE_OF_FILE)[0]
-                #if notebook_name[0] == "":
-#                 notebook_name = notebook_name[1:]
-                # Get Absolute path to import in Databricks Notebook
-                notebook_abs_path = os.path.join(NOTEBOOK_DIRECTORY,notebook_name).replace('\\','/')
-                #print("printing notebook 0",notebook_name[0],notebook_name[1:])
-                print("printint nbote booke abs path",notebook_abs_path)
-                # Get Absolute path of python file
-                abs_path_file = os.path.join(path,file)
-                #print("sending the notebookPath and abs Path",notebook_abs_path,abs_path_file)
-                response = import_to_databricks(notebook_abs_path,abs_path_file) 
+    # Add Modify and new file
+    print("Begin add file")
+    for file_to_add in to_add:
+        split_remove_path_add = pathlib.Path(file_to_add)
+        raw_path_add = pathlib.Path(*split_remove_path_add.parts[3:])
+        join_raw_path_add = os.path.join(NOTEBOOK_DIRECTORY, raw_path_add)
+        final_dir_add_raw = join_raw_path_add.split(TYPE_OF_FILE)[0]
+        print(final_dir_add_raw)
 
-                #print("the absolute Path",abs_path_file)
-                #print("the absolute Path on Server",notebook_abs_path)
+        file_to_add_raw = pathlib.Path(*split_remove_path_add.parts[1:])
+        full_file_path = os.path.join(DATABRICKS_NOTEBOOKS_DIRECTORY,file_to_add_raw)
+        response = import_to_databricks(final_dir_add_raw,full_file_path)
+
+
 else:
-    raise Exception("Cannot find directory: {}".format(DIRECTORY))
+    #raise Exception("Cannot find directory or empty folder: {}".format(DIRECTORY))
+    print("Cannot find directory or empty folder: {}".format(DIRECTORY))
