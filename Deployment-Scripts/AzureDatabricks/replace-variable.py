be3556e9155a695920c595565b87cb524d04c28b
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-08 16:33:01 +0700

    Update replace-variable.py

--- Deployment-Scripts/AzureDatabricks/replace-variable.py
+++ Deployment-Scripts/AzureDatabricks/replace-variable.py
@@ -7,16 +7,16 @@ key_func = str(sys.argv[2])
 key_class = str(sys.argv[3])
 key = []
 value = []
-key[0] = "${" + "function}"
+key[0] = "function"
 value[0] = key_func
-key[1] = "${" + "class}"
+key[1] = "class"
 value[1] = key_class
 
 def update_configValue(rootfilepath):
 
     with fileinput.FileInput(rootfilepath, inplace=True) as file:
        	for line in file:
-            print(line.replace(key[i], value[i].lower()), end='')
+            print(line.replace("$" + "{" + key[i] + "}", "$" + "{" + value[i].lower() + "}"), end='')
 
 for i in range(len(key)):
 	for subdir, file in os.walk(import_nb):
