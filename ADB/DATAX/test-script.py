fd9d1a2a9ff4c7fe12db282de6af46dbb511bf1b
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-08 11:31:39 +0700

    Update test-script.py

--- ADB/DATAX/test-script.py
+++ ADB/DATAX/test-script.py
@@ -15,10 +15,10 @@ for jar_file in jar_file_list:
 # COMMAND ----------
 
 # Encryption functions 
-spark.sql(f""" CREATE FUNCTION IF NOT EXISTS encrypt AS 'AESEncryptedUDF'
+spark.sql(f""" CREATE OR REPLACE FUNCTION encrypt AS 'AESEncryptedUDF'
                 USING JAR '/Jars/jars/{require_jar_dict["AESEncryption_UDF_V3_3_0"]}' """) 
 
-spark.sql(f""" CREATE FUNCTION IF NOT EXISTS decrypt AS 'AESDecryptedUDF'
+spark.sql(f""" CREATE OR REPLACE FUNCTION decrypt AS 'AESDecryptedUDF'
                 USING JAR '/Jars/jars/{require_jar_dict["AESEncryption_UDF_V3_3_0"]}' """)
 
 
