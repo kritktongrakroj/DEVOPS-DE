9c46b9ffe6229a5c9d8cfa2ebfa903e3f814775b
Isreeya Na <60770343+tnimeis7@users.noreply.github.com>
2022-09-08 17:06:13 +0700

    Update replace-variable.py

--- Deployment-Scripts/AzureDatabricks/replace-variable.py
+++ Deployment-Scripts/AzureDatabricks/replace-variable.py
@@ -1,7 +1,7 @@
-from jproperties import Properties
 import os
 import fileinput
 import sys
+configs = Properties()
 
 rootdir = str(sys.argv[1])
 key_func = str(sys.argv[2])
@@ -9,17 +9,6 @@ key_class = str(sys.argv[3])
 key = ["${function}","${class}"]
 value = [key_func,key_class]
 
-
-import os
-import fileinput
-import sys
-configs = Properties()
-
-prop_path= str(sys.argv[1])
-rootdir= str(sys.argv[2])
-
-#prop_path=r"/home/runner/work/dataengineering-devops/dataengineering-devops/DataFactory/npr-app-config.properties"
-#rootdir = r'/home/runner/work/dataengineering-devops/dataengineering-devops/DataFactory/'
 
 def update_configValue(rootfilepath,key,value):
 
